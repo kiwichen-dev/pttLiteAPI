87b673df250850db2aaf16c4aa32b5e7bf704354
kiwichen <kiwichen@tuta.io>
2020-11-19 17:49:50 +0800

    /hotbard -> /hotboard

--- restfulAPI/api/__init__.py
+++ restfulAPI/api/__init__.py
@@ -130,5 +130,5 @@ class App(InitAPP):
         api.add_resource(LogoutAccessToken, '/logout_access_token')
         api.add_resource(LogoutRefreshToken, '/logout_refresh_token')
         api.add_resource(Search, '/search')
-        api.add_resource(Top8AmountOfLikesBoards, '/hotbords')
+        api.add_resource(Top8AmountOfLikesBoards, '/hotboards')
         return app
